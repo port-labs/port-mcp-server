bc6a4c18384a9536c94946b63aa4a9513d81c832
Ivan Kalinovski <ivankalinovski@Ivans-MacBook-Pro.local>
2025-04-27 14:31:40 +0300

    Update json_schema_extra to be py dantic v2 compatable

--- src/models/common/base_pydantic.py
+++ src/models/common/base_pydantic.py
@@ -3,17 +3,10 @@ from typing import Any
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import ConfigDict as PydanticConfigDict
 
-
+def json_schema_extra(schema: dict[str, Any], model: Any) -> None:
+    schema.pop("title", None)
+    for prop in schema.get("properties", {}).values():
+        prop.pop("title", None)
+        
 class BaseModel(PydanticBaseModel):
-    model_config = PydanticConfigDict(validate_by_name=True, validate_by_alias=True, serialize_by_alias=True)
-
-    class ConfigDict:
-        @staticmethod
-        def json_schema_extra(schema: dict[str, Any], model: Any) -> None:
-            schema.pop("title", None)
-            if schema.get("default") is None:
-                schema.pop("default", None)
-            for prop in schema.get("properties", {}).values():
-                prop.pop("title", None)
-                if prop.get("default", None) is None:
-                    prop.pop("default", None)
+    model_config = PydanticConfigDict(validate_by_name=True, validate_by_alias=True, serialize_by_alias=True,json_schema_extra=json_schema_extra)
